f0144c76e39bc0bb860ef475bc6ef8abe1264693
Nadir Irshad <nadir.irshad@gmail.com>
2024-11-06 15:50:32 -0600

    img addition

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    if (name && email && message) {
-        alert(`Thank you, ${name}! Your message has been sent.`);
-    } else {
-        alert("Please fill in all fields.");
-    }
-});
+    const
